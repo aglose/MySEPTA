e09093bc5aeecc04049b67f32b6f6e6e40d44eed
Kevin Pintimalli <pintimallik1@gmail.com>
2015-09-16 16:08:23 -0400

    Test Commit Kevin 2.

--- app/src/main/java/team5/capstone/com/mysepta/MainActivity.java
+++ app/src/main/java/team5/capstone/com/mysepta/MainActivity.java
@@ -22,7 +22,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements ActionBar.TabListener {
-//test
+//tester
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
      * fragments for each of the sections. We use a
