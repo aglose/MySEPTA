855dbf37c50f99d954758c612f664bc335fdc0cf
Kevin Pintimalli <pintimallik1@gmail.com>
2015-10-26 21:59:23 -0400

    Added javadoc type comments.

--- app/src/main/java/team5/capstone/com/mysepta/Adapters/SubwayItineraryViewAdapter.java
+++ app/src/main/java/team5/capstone/com/mysepta/Adapters/SubwayItineraryViewAdapter.java
@@ -12,17 +12,29 @@ import team5.capstone.com.mysepta.Managers.SubwayScheduleManager;
 import team5.capstone.com.mysepta.R;
 
 /**
+ * Adapter for subway section of home rail screen. Dictates how subway itinerary fragment acts.
  * Created by Andrew on 9/20/2015.
  */
 public class SubwayItineraryViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private Context context;
     SubwayItineraryViewFragment.SubwayChangeFragmentListener mSubwayFragmentListener;
 
+    /**
+     * Constructor
+     * @param context activity
+     * @param mSubwayFragmentListener listener for button clicks
+     */
     public SubwayItineraryViewAdapter(Context context, SubwayItineraryViewFragment.SubwayChangeFragmentListener mSubwayFragmentListener) {
         this.context = context;
         this.mSubwayFragmentListener = mSubwayFragmentListener;
     }
 
+    /**
+     * Create recycler view holder for subway fragment. Set button click listeners.
+     * @param parent parent viewgroup
+     * @param viewType type of view
+     * @return recycler view holder
+     */
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(final ViewGroup parent, int viewType) {
         final View view = LayoutInflater.from(parent.getContext())
@@ -52,11 +64,20 @@ public class SubwayItineraryViewAdapter extends RecyclerView.Adapter<RecyclerVie
         };
     }
 
+    /**
+     * Empty method, needed for override
+     * @param holder recycler view holder
+     * @param position item position
+     */
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
 
     }
 
+    /**
+     * Returns item count.
+     * @return 1
+     */
     @Override
     public int getItemCount() {
         return 1;
--- app/src/main/java/team5/capstone/com/mysepta/Adapters/SubwayScheduleViewAdapter.java
+++ app/src/main/java/team5/capstone/com/mysepta/Adapters/SubwayScheduleViewAdapter.java
@@ -13,16 +13,27 @@ import team5.capstone.com.mysepta.Models.SubwayScheduleItemModel;
 import team5.capstone.com.mysepta.R;
 
 /**
+ * Adapter for subway schedule view.
  * Created by tiestodoe on 10/1/15.
  */
 public class SubwayScheduleViewAdapter extends RecyclerView.Adapter<SubwayScheduleViewAdapter.SubwayScheduleHolder> {
     private ArrayList<SubwayScheduleItemModel> arrivals;
 
 
+    /**
+     * Constructor
+     * @param arrivals list of arrival times for subway
+     */
     public SubwayScheduleViewAdapter(ArrayList<SubwayScheduleItemModel> arrivals){
         this.arrivals = arrivals;
     }
 
+    /**
+     * Create view holder
+     * @param parent parent view group
+     * @param viewType view type
+     * @return view holder
+     */
     @Override
     public SubwayScheduleHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
@@ -30,20 +41,36 @@ public class SubwayScheduleViewAdapter extends RecyclerView.Adapter<SubwaySchedu
         return new SubwayScheduleHolder(view);
     }
 
+    /**
+     * Initialize text in viewholder.
+     * @param holder view holder
+     * @param position current item position
+     */
     @Override
     public void onBindViewHolder(SubwayScheduleHolder holder, int position) {
         holder.scheduleText.setText(arrivals.get(position).getFormattedTimeStr());
     }
 
+    /**
+     * Get item count.
+     * @return size of arrivals array
+     */
     @Override
     public int getItemCount() {
         return arrivals.size();
     }
 
+    /**
+     * Subway schedule view holder
+     */
     public class SubwayScheduleHolder extends RecyclerView.ViewHolder{
         TextView scheduleText;
         CardView cardView;
 
+        /**
+         * Constructor
+         * @param itemView current item view
+         */
         public SubwayScheduleHolder(View itemView) {
             super(itemView);
             cardView = (CardView) itemView.findViewById(R.id.subway_schedule_item_card);
