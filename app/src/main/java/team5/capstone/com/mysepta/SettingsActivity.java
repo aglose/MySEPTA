07e9810920a501f5456b6927598b271458d7aa7c
mattarbiz <tud15899@temple.edu>
2015-11-18 10:01:19 -0500

    alerts alerts alerts. removed parameters, acquiring all alerts.

--- app/src/main/java/team5/capstone/com/mysepta/SettingsActivity.java
+++ app/src/main/java/team5/capstone/com/mysepta/SettingsActivity.java
@@ -1,7 +1,38 @@
 package team5.capstone.com.mysepta;
 
-/**
- * Created by Cathy on 11/16/2015.
- */
-public class SettingsActivity extends MainActivity {
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.widget.TextView;
+
+public class SettingsActivity extends PreferenceActivity {
+/*
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.layout.settings);
+        showUserSettings();
+
+    }
+
+    private void showUserSettings() {
+        SharedPreferences sharedPrefs = PreferenceManager
+                .getDefaultSharedPreferences(this);
+
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("\n Username: "
+                + sharedPrefs.getString("prefUsername", "NULL"));
+
+        builder.append("\n Send report:"
+                + sharedPrefs.getBoolean("prefSendReport", false));
+
+        builder.append("\n Sync Frequency: "
+                + sharedPrefs.getString("prefSyncFrequency", "NULL"));
+
+
+    }
+    */
 }
