33995b52a418801653d220ac4c3714eff974a268
mattarbiz <tud15899@temple.edu>
2015-12-02 15:59:58 -0500

    Missing alert resources, Fragments

---
+++ app/src/main/java/team5/capstone/com/mysepta/Fragment/GenAlertsFragment.java
@@ -0,0 +1,38 @@
+package team5.capstone.com.mysepta.Fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+import team5.capstone.com.mysepta.Adapters.AlertsAdapter;
+import team5.capstone.com.mysepta.Models.AlertsModel;
+import team5.capstone.com.mysepta.R;
+
+public class GenAlertsFragment extends Fragment {
+
+    private RecyclerView alertsRecyclerView;
+    private View view;
+    public static ArrayList<AlertsModel> generalList;
+
+    private AlertsAdapter alertsAdapter;
+
+    public GenAlertsFragment() {}
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_alerts, container, false);
+
+        alertsRecyclerView = (RecyclerView) view.findViewById(R.id.alertsRecyclerView);
+        alertsRecyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        generalList = new ArrayList<>();
+        alertsAdapter = new AlertsAdapter(generalList, view.getContext());
+        alertsRecyclerView.setAdapter(alertsAdapter);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/team5/capstone/com/mysepta/Fragment/SubAlertsFragment.java
@@ -0,0 +1,38 @@
+package team5.capstone.com.mysepta.Fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+import team5.capstone.com.mysepta.Adapters.AlertsAdapter;
+import team5.capstone.com.mysepta.Models.AlertsModel;
+import team5.capstone.com.mysepta.R;
+
+public class SubAlertsFragment extends Fragment {
+
+    private RecyclerView alertsRecyclerView;
+    private View view;
+    public static ArrayList<AlertsModel> subList;
+
+    private AlertsAdapter alertsAdapter;
+
+    public SubAlertsFragment() {}
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_alerts, container, false);
+
+        alertsRecyclerView = (RecyclerView) view.findViewById(R.id.alertsRecyclerView);
+        alertsRecyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        subList = new ArrayList<>();
+        alertsAdapter = new AlertsAdapter(subList, view.getContext());
+        alertsRecyclerView.setAdapter(alertsAdapter);
+
+        return view;
+    }
+}
